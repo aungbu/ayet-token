a228116d565e553c73e414f39ff951ecbca27fa7
George <38444456+aungbu@users.noreply.github.com>
2026-07-01 11:02:13 -0700

    Add files via upload

---
+++ AYET.sol
@@ -0,0 +1,71 @@
+pragma solidity 0.8.26;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/NoncesUpgradeable.sol";
+
+contract AYET is
+    Initializable,
+    ERC20Upgradeable,
+    ERC20BurnableUpgradeable,
+    PausableUpgradeable,
+    OwnableUpgradeable,
+    ERC20PermitUpgradeable,
+    ERC20VotesUpgradeable,
+    UUPSUpgradeable
+{
+
+    uint256 public constant INITIAL_SUPPLY = 4_000_000_000 * 10 ** 18;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner, address initialReceiver) public initializer {
+        require(initialOwner != address(0), "AYET: owner is zero address");
+        require(initialReceiver != address(0), "AYET: receiver is zero address");
+
+        __ERC20_init("AKIMOTO YASUSHI ENTERTAINMENT TOKEN", "AYET");
+        __ERC20Burnable_init();
+        __Pausable_init();
+        __Ownable_init(initialOwner);
+        __ERC20Permit_init("AKIMOTO YASUSHI ENTERTAINMENT TOKEN");
+        __ERC20Votes_init();
+        __UUPSUpgradeable_init();
+
+        _mint(initialReceiver, INITIAL_SUPPLY);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+        whenNotPaused
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20PermitUpgradeable, NoncesUpgradeable)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
